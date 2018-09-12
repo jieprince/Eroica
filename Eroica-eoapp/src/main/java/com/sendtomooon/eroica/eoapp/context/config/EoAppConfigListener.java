8062c2ffdc3e2a359ec52b77d83bd81ddccc182a
galaxysha512 <galaxysha512@gmail.com>
2018-09-12 09:59:46 +0800

    增加一个配置监听的功能

---
+++ Eroica-eoapp/src/main/java/com/sendtomooon/eroica/eoapp/context/config/EoAppConfigListener.java
@@ -0,0 +1,63 @@
+package com.sendtomooon.eroica.eoapp.context.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.pingan.pafa.pizza.classloader.PizzaURL;
+import com.pingan.pafa.pizza.spring.PizzaResourceListener;
+import com.sendtomooon.eroica.eoapp.context.event.EoAppConfigChangedEvent;
+
+public class EoAppConfigListener implements PizzaResourceListener, ApplicationContextAware {
+
+	protected Log logger = LogFactory.getLog(this.getClass());
+
+	private EoAppConfigProperties configProperties;
+
+	@Override
+	public void onChanged(PizzaURL pizzaURL, InputStream content) {
+		if (content != null) {
+			try {
+				configProperties.refresh(content);
+				if (logger.isInfoEnabled()) {
+					logger.info("EoAppConfigChanged:" + configProperties.toString());
+				}
+				applicationContext.publishEvent(new EoAppConfigChangedEvent(applicationContext, configProperties));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.applicationContext = (ConfigurableApplicationContext) context;
+	}
+
+	private ConfigurableApplicationContext applicationContext;
+
+	@Override
+	public PizzaURL getPizzaURL() {
+		return configProperties.getPizzaURL();
+	}
+
+	@Override
+	public boolean isListenEnable() {
+		return configProperties.getProperty(EoAppConstants.KEY_PIZZA_LISTENER_ENABLE, false);
+	}
+
+	public EoAppConfigProperties getConfigProperties() {
+		return configProperties;
+	}
+
+	public void setConfigProperties(EoAppConfigProperties configProperties) {
+		this.configProperties = configProperties;
+	}
+
+}
