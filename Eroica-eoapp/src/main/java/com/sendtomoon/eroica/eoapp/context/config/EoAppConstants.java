268a5126f0147c702676fcd0a26c954251de583c
galaxysha512 <galaxysha512@gmail.com>
2018-10-01 09:50:40 +0800

    字典类

---
+++ Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/context/config/EoAppConstants.java
@@ -0,0 +1,35 @@
+package com.sendtomoon.eroica.eoapp.context.config;
+
+public abstract class EoAppConstants {
+
+	public final static String KEY_SAR_LIST="eoapp.sar.list";
+	
+	public static final String KEY_PROTOCOLS="eoapp.protocols";
+	
+	public static final String KEY_HTTP_ESA_ENABLE="eoapp.httpesa.enable";
+	
+	public static final String KEY_LIB_LIST="eoapp.lib.list";
+	
+	public static final String KEY_LIB_DIR="eoapp.lib.dir";
+	
+	public static final String KEY_POLA_CONFIG="pola.config";
+	
+	public static final String KEY_WORK_DIR="eoapp.work.dir";
+	
+	public static final String KEY_WEB_ENABLE="eoapp.web.enable";
+	
+	public static final String KEY_CAT_WEB_ENABLE="eoapp.cat.web.enable";
+	
+	public static final String KEY_PLUGINS="eoapp.plugins";
+	
+	public static final String KEY_WEB_DISPATHER="eoapp.web.dispatcher";
+	
+	public static final String KEY_RESOURCES_SUFFIX="eoapp.resources.suffix";
+	
+	public static final String KEY_PIZZA_LISTENER_ENABLE="eoapp.pizzalistener.enable";
+	
+	public static final String KEY_WEB_ROOT="eoapp.web.root";
+	
+	public static final String KEY_HTTP_ESA_ENABLE_PAFA52="eoapp.esa.http.export.enable";
+	
+}
