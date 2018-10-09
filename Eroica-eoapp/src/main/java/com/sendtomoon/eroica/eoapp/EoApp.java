54262bf98476f70ab2dbc0ac06bd7b4c69f08c78
galaxysha512 <galaxysha512@gmail.com>
2018-10-09 17:31:42 +0800

    基础类

---
+++ Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/EoApp.java
@@ -0,0 +1,21 @@
+package com.sendtomoon.eroica.eoapp;
+
+import com.sendtomoon.eroica.pizza.Pizza;
+
+/**
+ * 引导程序之一，启动组件之一
+ * @author lbt42
+ *
+ */
+public abstract class EoApp implements EoAppContext {
+
+	private volatile static EoApp eoApp;
+
+	public synchronized static EoApp getInstance() {
+		if (eoApp == null) {
+			eoApp = Pizza.getSpringContext().getBean(EoApp.class);
+		}
+		return eoApp;
+	}
+
+}
