7b2bcf679330b2fcab10ed9ae562c9ed27281655
galaxysha512 <galaxysha512@gmail.com>
2018-10-10 11:15:59 +0800

    commit

---
+++ Eroica-eoapp/src/test/java/com/sendtomoon/eroica/test/TestBean2.java
@@ -0,0 +1,11 @@
+package com.sendtomoon.eroica.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean2 {
+
+	public String hello(){
+		return "Eroica!";
+	}
+}
