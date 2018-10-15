3a63f0e4f29d51507835b49db02086ca47405047
galaxysha512 <galaxysha512@gmail.com>
2018-10-15 13:39:18 +0800

    class load factory

---
+++ Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/context/lib/EoAppClassLoaderFactory.java
@@ -0,0 +1,8 @@
+package com.sendtomoon.eroica.eoapp.context.lib;
+
+import com.sendtomoon.eroica.pizza.classloader.PizzaClassLoader;
+
+public interface EoAppClassLoaderFactory{
+	
+	public PizzaClassLoader getClassLoader();
+}
