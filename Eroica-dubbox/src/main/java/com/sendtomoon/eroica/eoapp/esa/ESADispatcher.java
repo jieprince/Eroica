44a703c040370cf0bede52235decff43ab4b81d1
galaxysha512 <galaxysha512@gmail.com>
2018-11-12 15:31:48 +0800

    ESA解析器

---
+++ Eroica-dubbox/src/main/java/com/sendtomoon/eroica/eoapp/esa/ESADispatcher.java
@@ -0,0 +1,8 @@
+package com.sendtomoon.eroica.eoapp.esa;
+
+import com.sendtomoon.eroica.common.app.biz.ac.ApplicationController;
+
+public interface ESADispatcher extends ApplicationController,ESADefinitionsResolver {
+	
+	
+}
