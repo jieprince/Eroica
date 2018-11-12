fb87b7502da98d003b0449d691509ec9ab56ae90
galaxysha512 <galaxysha512@gmail.com>
2018-11-12 15:05:21 +0800

    dubbo组件

---
+++ Eroica-dubbox/src/main/java/com/sendtomoon/dubbo/registry/ConsulRegistryFactory.java
@@ -0,0 +1,12 @@
+package com.sendtomoon.dubbo.registry;
+/*package com.pingan.dubbo.registry;
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.registry.Registry;
+import com.alibaba.dubbo.registry.support.AbstractRegistryFactory;
+
+public class ConsulRegistryFactory extends AbstractRegistryFactory {
+	protected Registry createRegistry(URL url) {
+		return new ConsulRegistry(url);
+	}
+}*/
