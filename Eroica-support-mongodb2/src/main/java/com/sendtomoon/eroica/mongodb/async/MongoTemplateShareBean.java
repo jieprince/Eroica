22046bcd68e1dad72b91817576815c17e84658da
galaxysha512 <galaxysha512@gmail.com>
2019-01-22 11:46:36 +0800

    commit

---
+++ Eroica-support-mongodb2/src/main/java/com/sendtomoon/eroica/mongodb/async/MongoTemplateShareBean.java
@@ -0,0 +1,25 @@
+package com.sendtomoon.eroica.mongodb.async;
+
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+
+import com.sendtomoon.eroica.common.utils.PURL;
+import com.sendtomoon.eroica.pizza.classloader.PizzaURL;
+import com.sendtomoon.eroica.pizza.spring.PizzaResource;
+import com.sendtomoon.eroica.pizza.spring.schema.PizzaShareBean;
+
+public class MongoTemplateShareBean implements PizzaShareBean {
+
+	@Override
+	public BeanDefinition create(PizzaURL pizzaURL, PURL configURL) {
+		RootBeanDefinition beanDefinition = new RootBeanDefinition();
+		beanDefinition.setLazyInit(configURL.getParameter("lazy", false));
+		beanDefinition.setBeanClass(MongoTemplateFactoryBean.class);
+		MutablePropertyValues pvs = beanDefinition.getPropertyValues();
+		pvs.addPropertyValue("configureResource", new PizzaResource(pizzaURL));
+		pvs.addPropertyValue("initializeOnStartup", configURL.getParameter("initializeOnStartup", true));
+		return beanDefinition;
+	}
+
+}
