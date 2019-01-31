3c26761d3ac61b550c8c274434fbf2b673001242
galaxysha512 <galaxysha512@gmail.com>
2019-01-31 14:40:19 +0800

    fix

--- Eroica-fling/src/test/java/com/sendtomoon/eroica/fling/test/SARTests.java
+++ Eroica-fling/src/test/java/com/sendtomoon/eroica/fling/test/SARTests.java
@@ -2,8 +2,8 @@ package com.sendtomoon.eroica.fling.test;
 
 import org.junit.Test;
 
-import com.pingan.pafa.papp.test.BaseSARTest;
-import com.pingan.pafa.papp.test.SARContextConfiguration;
+import com.sendtomoon.eroica.eoapp.test.BaseSARTest;
+import com.sendtomoon.eroica.eoapp.test.SARContextConfiguration;
 
 @SARContextConfiguration(plugins = "dubbo,jetty")
 public class SARTests extends BaseSARTest {
