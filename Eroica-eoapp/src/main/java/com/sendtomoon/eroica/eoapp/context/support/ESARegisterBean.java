e3672602cd7876eaf9ce2670d23905e85af641d7
galaxysha512 <galaxysha512@gmail.com>
2019-02-28 13:36:32 +0800

    fix

--- Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/context/support/ESARegisterBean.java
+++ Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/context/support/ESARegisterBean.java
@@ -7,31 +7,27 @@ import com.sendtomoon.eroica.eoapp.sar.event.SAREvent;
 import com.sendtomoon.eroica.eoapp.sar.event.SARShutdownEvent;
 import com.sendtomoon.eroica.eoapp.sar.event.SARStartupedEvent;
 
-public class ESARegisterBean implements ApplicationListener<SAREvent>{
-	
+public class ESARegisterBean implements ApplicationListener<SAREvent> {
+
 	private ESAManager esaManager;
-	
-	
+
 	@Override
 	public void onApplicationEvent(SAREvent event) {
-		if(event instanceof SARStartupedEvent){
-			SARContext sarContext=event.getSARContext();
+		if (event instanceof SARStartupedEvent) {
+			SARContext sarContext = event.getSARContext();
 			esaManager.export(sarContext);
-		}else if(event instanceof SARShutdownEvent){
-			SARContext sarContext=event.getSARContext();
+		} else if (event instanceof SARShutdownEvent) {
+			SARContext sarContext = event.getSARContext();
 			esaManager.unexport(sarContext);
 		}
 	}
-
 
 	public ESAManager getEsaManager() {
 		return esaManager;
 	}
 
-
 	public void setEsaManager(ESAManager esaManager) {
 		this.esaManager = esaManager;
 	}
-	
-	
+
 }
