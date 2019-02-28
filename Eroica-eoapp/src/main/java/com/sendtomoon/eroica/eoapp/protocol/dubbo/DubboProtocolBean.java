bc173473919b51edb15d5d466ccebd10197ba4ce
galaxysha512 <galaxysha512@gmail.com>
2019-02-28 10:29:32 +0800

    fix

--- Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/protocol/dubbo/DubboProtocolBean.java
+++ Eroica-eoapp/src/main/java/com/sendtomoon/eroica/eoapp/protocol/dubbo/DubboProtocolBean.java
@@ -24,39 +24,38 @@ import com.alibaba.dubbo.config.ProviderConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.ServiceConfig;
 import com.alibaba.dubbo.rpc.service.GenericService;
-import com.sendtomoon.eroica.ac.dubbo.PafaAcGenericService;
+import com.sendtomoon.eroica.ac.dubbo.EroicaAcGenericService;
 import com.sendtomoon.eroica.common.exception.EroicaException;
 import com.sendtomoon.eroica.eoapp.esa.ESADefinition;
 import com.sendtomoon.eroica.eoapp.protocol.ESAExporter;
 import com.sendtomoon.eroica.eoapp.protocol.dubbo.ws.WebGenericService;
 import com.sendtomoon.eroica.eoapp.sar.SARContext;
 
-public class DubboProtocolBean extends ApplicationObjectSupport  implements InitializingBean,IDubboProtocol
-	,ESAExporter,DisposableBean,ServletContextAware {
+public class DubboProtocolBean extends ApplicationObjectSupport
+		implements InitializingBean, IDubboProtocol, ESAExporter, DisposableBean, ServletContextAware {
 
+	private Map<String, ServiceConfig<GenericService>> _configs = new ConcurrentHashMap<String, ServiceConfig<GenericService>>();
 
-	private Map<String,ServiceConfig<GenericService>> _configs=new ConcurrentHashMap<String, ServiceConfig<GenericService>>();
-	
-	protected Log logger=LogFactory.getLog(this.getClass());
-	
+	protected Log logger = LogFactory.getLog(this.getClass());
+
 	private ServletContext servletContext;
 
-	public DubboProtocolBean(){}
-	
+	public DubboProtocolBean() {
+	}
+
 	private List<RegistryConfig> defaultRegistryConfigs;
-	
-	private   Map<String, RegistryConfig> registryConfigs;
-	
-	
+
+	private Map<String, RegistryConfig> registryConfigs;
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		Map<String, RegistryConfig> registryConfigMap = BeanFactoryUtils.beansOfTypeIncludingAncestors(
-				this.getApplicationContext(), RegistryConfig.class, false, false);
+		Map<String, RegistryConfig> registryConfigMap = BeanFactoryUtils
+				.beansOfTypeIncludingAncestors(this.getApplicationContext(), RegistryConfig.class, false, false);
 		List<RegistryConfig> defaultRegistryConfigs = new ArrayList<RegistryConfig>();
 		// --------------------------------------------------------------
 		if (registryConfigMap == null) {
 			registryConfigMap = new LinkedHashMap<String, RegistryConfig>();
-		}else{
+		} else {
 			for (RegistryConfig config : registryConfigMap.values()) {
 				if (config.isDefault() != null && config.isDefault().booleanValue()) {
 					defaultRegistryConfigs.add(config);
@@ -72,95 +71,79 @@ public class DubboProtocolBean extends ApplicationObjectSupport  implements Init
 		this.defaultRegistryConfigs = defaultRegistryConfigs;
 		this.registryConfigs = registryConfigMap;
 	}
-	
 
-	public boolean export(SARContext context,ESADefinition definition) {
-		String esaName=definition.getEsaName();
-		ServiceConfig<GenericService> temp=_configs.get(esaName);
-		if(temp!=null){
-			try{
+	public boolean export(SARContext context, ESADefinition definition) {
+		String esaName = definition.getEsaName();
+		ServiceConfig<GenericService> temp = _configs.get(esaName);
+		if (temp != null) {
+			try {
 				temp.unexport();
-			}catch(Throwable th){
-				logger.error("ESA<"+esaName+"> unexport error:"+th.getMessage(),th);
+			} catch (Throwable th) {
+				logger.error("ESA<" + esaName + "> unexport error:" + th.getMessage(), th);
 			}
 		}
-		try{
-			doExport(esaName,context,definition.getProperties());
-		}catch(Throwable th){
-			throw new EroicaException("ESA<"+esaName+"> export error:"+th.getMessage(),th);
+		try {
+			doExport(esaName, context, definition.getProperties());
+		} catch (Throwable th) {
+			throw new EroicaException("ESA<" + esaName + "> export error:" + th.getMessage(), th);
 		}
 		return true;
 	}
 
-	
-	protected void doExport(String esaName,SARContext context, Properties configProperties){
-		ServiceConfig<GenericService> serviceConfig=new ServiceConfig<GenericService>();
+	protected void doExport(String esaName, SARContext context, Properties configProperties) {
+		ServiceConfig<GenericService> serviceConfig = new ServiceConfig<GenericService>();
 		serviceConfig.appendProperties(configProperties);
-		serviceConfig.setInterface(esaName); 
-		serviceConfig.setRef(createGenericService(context,configProperties));
-		serviceConfig.setProvider(getProviderConfig(context.getSARName(),configProperties));
-		
-		//dubboConfig.setModule(module)
-		//--------------------------
+		serviceConfig.setInterface(esaName);
+		serviceConfig.setRef(createGenericService(context, configProperties));
+		serviceConfig.setProvider(getProviderConfig(context.getSARName(), configProperties));
+
+		// dubboConfig.setModule(module)
+		// --------------------------
 		serviceConfig.export();
 		_configs.put(esaName, serviceConfig);
 	}
-	
-	protected ProviderConfig getProviderConfig(String sarName,Properties configProperties){
-		ApplicationConfig aplication=new ApplicationConfig();
+
+	protected ProviderConfig getProviderConfig(String sarName, Properties configProperties) {
+		ApplicationConfig aplication = new ApplicationConfig();
 		aplication.setName(sarName);
-		ProviderConfig providerConfig=new ProviderConfig();
+		ProviderConfig providerConfig = new ProviderConfig();
 		providerConfig.setApplication(aplication);
 		//
-		providerConfig.setRegistries(getRegistryConfig(
-				StringUtils.tokenizeToStringArray(configProperties.getProperty("registry"), ",")));
-		providerConfig.setProtocols(getProtocolConfig(
-				StringUtils.tokenizeToStringArray(configProperties.getProperty("protocol"), ",")));
+		providerConfig.setRegistries(
+				getRegistryConfig(StringUtils.tokenizeToStringArray(configProperties.getProperty("registry"), ",")));
+		providerConfig.setProtocols(
+				getProtocolConfig(StringUtils.tokenizeToStringArray(configProperties.getProperty("protocol"), ",")));
 		return providerConfig;
 	}
-	
-	
-	protected GenericService createGenericService(SARContext context, Properties configProperties){
-		String mappingPath=configProperties.getProperty(ESADefinition.WEB_MAPPING_PATH);
-		if(mappingPath!=null && (mappingPath=mappingPath.trim()).length()>0){
-			return new WebGenericService(context,servletContext,mappingPath);
-		}else{
-			return new PafaAcGenericService(context);
+
+	protected GenericService createGenericService(SARContext context, Properties configProperties) {
+		String mappingPath = configProperties.getProperty(ESADefinition.WEB_MAPPING_PATH);
+		if (mappingPath != null && (mappingPath = mappingPath.trim()).length() > 0) {
+			return new WebGenericService(context, servletContext, mappingPath);
+		} else {
+			return new EroicaAcGenericService(context);
 		}
 	}
-	
 
-	
+	/*
+	 * @Override public boolean export(String providerName,ESAConfig... configures)
+	 * { if(configures==null || configures.length==0)return false; for(ESAConfig
+	 * config:configures){ this.export(providerName,config); } return true; }
+	 * 
+	 * 
+	 * @Override public boolean unexport(String providerName,String... esaNames) {
+	 * if(esaNames==null || esaNames.length==0)return false; for(String
+	 * esaName:esaNames){ this.unexport(esaName); } return true; }
+	 */
 
-
-	/*@Override
-	public boolean export(String providerName,ESAConfig... configures) {
-		if(configures==null || configures.length==0)return false;
-		for(ESAConfig config:configures){
-			this.export(providerName,config);
-		}
-		return true;
-	}
-
-
-	@Override
-	public boolean unexport(String providerName,String... esaNames) {
-		if(esaNames==null || esaNames.length==0)return false;
-		for(String esaName:esaNames){
-			this.unexport(esaName);
-		}
-		return true;
-	}*/
-
-
-	public boolean unexport(SARContext context,String esaName) {
-		ServiceConfig<GenericService> temp=_configs.remove(esaName);
-		if(temp!=null){
-			try{
+	public boolean unexport(SARContext context, String esaName) {
+		ServiceConfig<GenericService> temp = _configs.remove(esaName);
+		if (temp != null) {
+			try {
 				temp.unexport();
 				return true;
-			}catch(Throwable th){
-				logger.error("ESA<"+esaName+"> unexport error:"+th.getMessage(),th);
+			} catch (Throwable th) {
+				logger.error("ESA<" + esaName + "> unexport error:" + th.getMessage(), th);
 				return false;
 			}
 		}
@@ -168,43 +151,40 @@ public class DubboProtocolBean extends ApplicationObjectSupport  implements Init
 	}
 
 	public final void destroy() {
-		Object [] keys=_configs.keySet().toArray();
-		for(Object esaName:keys){
-			ServiceConfig<GenericService> temp=_configs.remove((String)esaName);
-			if(temp!=null){
-				try{
+		Object[] keys = _configs.keySet().toArray();
+		for (Object esaName : keys) {
+			ServiceConfig<GenericService> temp = _configs.remove((String) esaName);
+			if (temp != null) {
+				try {
 					temp.unexport();
-				}catch(Throwable th){
-					logger.error("ESA<"+esaName+"> unexport error:"+th.getMessage(),th);
+				} catch (Throwable th) {
+					logger.error("ESA<" + esaName + "> unexport error:" + th.getMessage(), th);
 				}
 			}
 		}
 		_configs.clear();
 		onDestroy();
 	}
-	
-	protected  void onDestroy(){
+
+	protected void onDestroy() {
 	}
 
 	@Override
 	public void setServletContext(ServletContext servletContext) {
-		this.servletContext=servletContext;
+		this.servletContext = servletContext;
 	}
-
-	
-
 
 	@Override
 	public List<RegistryConfig> getRegistryConfig(String[] registryIds) {
-		if(registryIds==null || registryIds.length==0){
+		if (registryIds == null || registryIds.length == 0) {
 			return this.defaultRegistryConfigs;
 		}
-		List<RegistryConfig> configs=new ArrayList<RegistryConfig>();
-		for(String id:registryIds){
-			if(id!=null && (id=id.trim()).length()>0){
-				RegistryConfig config=registryConfigs.get(id);
-				if(config==null){
-					throw new NullPointerException("Not found registry<"+id+">.");
+		List<RegistryConfig> configs = new ArrayList<RegistryConfig>();
+		for (String id : registryIds) {
+			if (id != null && (id = id.trim()).length() > 0) {
+				RegistryConfig config = registryConfigs.get(id);
+				if (config == null) {
+					throw new NullPointerException("Not found registry<" + id + ">.");
 				}
 				configs.add(config);
 			}
@@ -212,24 +192,18 @@ public class DubboProtocolBean extends ApplicationObjectSupport  implements Init
 		return configs;
 	}
 
-
 	@Override
 	public List<ProtocolConfig> getProtocolConfig(String[] protocolIds) {
-		if(protocolIds==null || protocolIds.length==0){
+		if (protocolIds == null || protocolIds.length == 0) {
 			return null;
 		}
-		List<ProtocolConfig> configs=new ArrayList<ProtocolConfig>();
-		for(String id:protocolIds){
-			if(id!=null && (id=id.trim()).length()>0){
+		List<ProtocolConfig> configs = new ArrayList<ProtocolConfig>();
+		for (String id : protocolIds) {
+			if (id != null && (id = id.trim()).length() > 0) {
 				configs.add(new ProtocolConfig(id));
 			}
 		}
-		return configs.size()>0?configs:null;
+		return configs.size() > 0 ? configs : null;
 	}
 
-
-
-	
-	
-	
 }
