c1f2e4a40fd63349f63b27595d0fa78f7e33ceb1
galaxysha512 <galaxysha512@gmail.com>
2019-03-04 16:16:18 +0800

    fix

--- Eroica-support/src/main/java/com/sendtomoon/eroica/datasource/monitor/MessageUtil.java
+++ Eroica-support/src/main/java/com/sendtomoon/eroica/datasource/monitor/MessageUtil.java
@@ -4,56 +4,56 @@ import java.util.Date;
 import java.util.UUID;
 
 public final class MessageUtil {
-	
+
 	public static String generateMsgId() {
 		return UUID.randomUUID().toString().replaceAll("-", "");
 	}
-	
+
 	public static String getStringValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return null;
 		} else {
-			return (String)obj;
+			return (String) obj;
 		}
 	}
-	
+
 	public static int getIntValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return 0;
 		} else {
-			return (Integer)obj;
+			return (Integer) obj;
 		}
 	}
-	
+
 	public static long getLongValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return 0L;
 		} else {
-			return (Long)obj;
+			return (Long) obj;
 		}
 	}
-	
+
 	public static boolean getBooleanValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return false;
 		} else {
-			return (Boolean)obj;
+			return (Boolean) obj;
 		}
 	}
-	
+
 	public static Date getDateValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return null;
 		} else {
-			return (Date)obj;
+			return (Date) obj;
 		}
 	}
-	
+
 	public static long[] getLongArrayValue(Object obj) {
-		if(obj == null) {
+		if (obj == null) {
 			return null;
 		} else {
-			return (long[])obj;
+			return (long[]) obj;
 		}
 	}
 
